eae64cb36f47b7874dc3e99d236e0cbde626f578
Sophie Clark <sophie.clark28@live.co.uk>
2019-05-23 09:57:15 +0100

    create app project

---
+++ RecipeApp/RecipeApp/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeApp
+//
+//  Created by Sophie Clark on 23/05/2019.
+//  Copyright © 2019 Sophie Clark. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
